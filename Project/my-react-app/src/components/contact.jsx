aa15a6567842dbc05184cb17b0df255159d6e41e
Thinal Dilmith <thinaldilmith3@gmail.com>
2026-01-03 08:52:10 +0530

    Add Contact section with Get In Touch content
    Implemented the Contact section UI including heading, descriptive text, social media icons, and a contact form with name, email, and message fields. Added user-friendly messaging to encourage inquiries and improve engagement.

---
+++ Project/my-react-app/src/components/contact.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "../style/contact.css";
+
+const Contact = () => {
+
+    return(
+
+        <section className="contact" id="contact-id">
+
+            <div className="contact-wrapper">
+
+                <div className="contact-text">
+
+                    <h4 className="contact-upper-title">Contact</h4>
+                    <h2 className="contact-title">Get In Touch</h2>
+
+                    <p className="contact-para"> 
+                        Contact us today for personalized property solutions. We’re here to provide expert guidance and trusted real estate advice.
+                    </p>
+
+                    <div className="contact-socials">
+                        <span>
+                            <svg viewBox="0 0 24 24" fill="currentColor"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>
+                        </span>
+
+                        <span>
+                            <svg viewBox="0 0 24 24" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
+                        </span>
+
+                        <span>
+                            <svg viewBox="0 0 24 24" fill="currentColor"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 1.366.062 2.633.332 3.608 1.308.975.975 1.245 2.242 1.308 3.608.058 1.266.07 1.646.07 4.85s-.012 3.584-.07 4.85c-.062 1.366-.332 2.633-1.308 3.608-.975.975-2.242 1.245-3.608 1.308-1.266.058-1.646.07-4.85.07s-3.584-.012-4.85-.07c-1.366-.062-2.633-.332-3.608-1.308-.975-.975-1.245-2.242-1.308-3.608-.058-1.266-.07-1.646-.07-4.85s.012-3.584.07-4.85c.062-1.366.332-2.633 1.308-3.608.975-.975 2.242-1.245 3.608-1.308 1.266-.058 1.646-.07 4.85-.07zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.355 2.618 6.778 6.98 6.978 1.28.058 1.688.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.668-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.845a1.44 1.44 0 1 0 0 2.881 1.44 1.44 0 0 0 0-2.881z"/></svg>
+                        </span>
+                        
+                    </div>
+
+                </div>
+
+                <form className="contact-form">
+
+                    <div className="form-group">
+
+                        <label>NAME</label>
+                        <input type="text" />
+                    </div>
+                
+
+                    <div className="form-group">
+                        <label>EMAIL ADDRESS</label>
+                        <input type="email" />
+                    </div>
+
+
+                    <div className="form-group">
+                        <label>MESSAGE</label>
+                        <textarea rows="3"></textarea>
+                    </div>
+
+                    <button type="submit" className="submit-btn">
+                        SUBMIT
+                    </button>
+
+                </form>
+
+            </div>
+
+        </section>
+    );
+}
+
+export default Contact;
