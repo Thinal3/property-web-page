26a9d3363b5fdfd2371bb98495a5385e0a00ad85
Thinal Dilmith <thinaldilmith3@gmail.com>
2026-01-05 03:52:21 +0530

    Update App.jsx
    replace browserRouter with  HashRouter

--- Project/my-react-app/src/App.jsx
+++ Project/my-react-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {  HashRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/nav';
 import Footer from './components/Footer';
 import SearchPage from './pages/searchResult'; 
